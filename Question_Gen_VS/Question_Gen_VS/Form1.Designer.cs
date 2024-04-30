629bfd962a32d8e760ba303a00dfd0ca2f6560b8
Bruno F. <95595686+brf-f@users.noreply.github.com>
2024-04-30 16:31:12 +0200

    added first elements

--- Question_Gen_VS/Question_Gen_VS/Form1.Designer.cs
+++ Question_Gen_VS/Question_Gen_VS/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Subject = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Subject
+            // 
+            this.Subject.AutoSize = true;
+            this.Subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subject.Location = new System.Drawing.Point(225, 9);
+            this.Subject.Name = "Subject";
+            this.Subject.Size = new System.Drawing.Size(322, 95);
+            this.Subject.TabIndex = 0;
+            this.Subject.Text = "Subject";
+            this.Subject.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 162);
+            this.label1.MaximumSize = new System.Drawing.Size(757, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(757, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Question goes here, lorem ipsum dolor sit amen?";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 341);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Subject);
+            this.Name = "Form1";
+            this.Text = "Question Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Subject;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Question_Gen_VS/Question_Gen_VS/Form1.cs
+++ Question_Gen_VS/Question_Gen_VS/Form1.cs
@@ -10,11 +10,38 @@ using System.Windows.Forms;
 
 namespace Question_Gen_VS
 {
+    class Question
+    {
+        String subject = "Computer Science";
+        String question = "Question";
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Question question = new Question();
+            Subject.Text = question.subject;
+        }
+
+        private void subject_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void question_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
