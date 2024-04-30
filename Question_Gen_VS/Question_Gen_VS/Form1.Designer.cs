b4a8f7116dd7257e9221b086db5eb5dcdba62f58
Bruno F. <95595686+brf-f@users.noreply.github.com>
2024-04-30 20:20:31 +0200

    Added functionality
    + added working array 'database' QuestionBank system
    + Improved Scaling and look

--- Question_Gen_VS/Question_Gen_VS/Form1.Designer.cs
+++ Question_Gen_VS/Question_Gen_VS/Form1.Designer.cs
@@ -28,52 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Subject = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Subject = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // Subject
-            // 
-            this.Subject.AutoSize = true;
-            this.Subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Subject.Location = new System.Drawing.Point(225, 9);
-            this.Subject.Name = "Subject";
-            this.Subject.Size = new System.Drawing.Size(322, 95);
-            this.Subject.TabIndex = 0;
-            this.Subject.Text = "Subject";
-            this.Subject.Click += new System.EventHandler(this.label1_Click);
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 162);
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 6, 10, 6);
             this.label1.MaximumSize = new System.Drawing.Size(757, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(757, 39);
+            this.label1.Size = new System.Drawing.Size(756, 174);
             this.label1.TabIndex = 1;
             this.label1.Text = "Question goes here, lorem ipsum dolor sit amen?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Subject, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(123, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(546, 107);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // Subject
+            // 
+            this.Subject.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Subject.AutoSize = true;
+            this.Subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Subject.Location = new System.Drawing.Point(22, 6);
+            this.Subject.Margin = new System.Windows.Forms.Padding(22, 6, 0, 0);
+            this.Subject.MaximumSize = new System.Drawing.Size(500, 95);
+            this.Subject.Name = "Subject";
+            this.Subject.Size = new System.Drawing.Size(500, 95);
+            this.Subject.TabIndex = 0;
+            this.Subject.Text = "Subject";
+            this.Subject.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Subject.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.BackColor = System.Drawing.Color.PowderBlue;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 143);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(776, 186);
+            this.tableLayoutPanel2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 341);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Subject);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(818, 388);
+            this.MinimumSize = new System.Drawing.Size(818, 388);
             this.Name = "Form1";
             this.Text = "Question Generator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Subject;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label Subject;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
     }
 }
 
--- Question_Gen_VS/Question_Gen_VS/Form1.cs
+++ Question_Gen_VS/Question_Gen_VS/Form1.cs
@@ -10,12 +10,6 @@ using System.Windows.Forms;
 
 namespace Question_Gen_VS
 {
-    class Question
-    {
-        String subject = "Computer Science";
-        String question = "Question";
-    }
-
     public partial class Form1 : Form
     {
         public Form1()
@@ -25,8 +19,8 @@ namespace Question_Gen_VS
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Question question = new Question();
-            Subject.Text = question.subject;
+            Question current_question = new Question();
+            Subject.Text = current_question.subject;
         }
 
         private void subject_Click(object sender, EventArgs e)
@@ -34,14 +28,42 @@ namespace Question_Gen_VS
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void question_Click(object sender, EventArgs e)
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
     }
+
+    class Question
+    {
+        private static Random rnd = new Random();
+        private static string[,] QuestionBank =
+        {
+        {"Physics", "question1"},
+        {"Computer Science", "question2"},
+        {"Maths", "question3"},
+        {"Geo", "question4"}
+    };
+
+        public string subject;
+        public string question;
+
+        public Question()
+        {
+            int num = rnd.Next(0, 4);
+            subject = QuestionBank[num, 0];
+            question = QuestionBank[num, 1];
+        }
+    }
+
 }
