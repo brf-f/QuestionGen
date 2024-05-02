bee79af732e0539aa3f66ee126275041a2bc3a39
Bruno F. <95595686+brf-f@users.noreply.github.com>
2024-05-02 11:48:24 +0200

    CompSci questions + features
    + Added reload button
    + Added first questions: CompSci past papers

--- Question_Gen_VS/Question_Gen_VS/Form1.Designer.cs
+++ Question_Gen_VS/Question_Gen_VS/Form1.Designer.cs
@@ -28,12 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Question = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.Subject = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.reloadImage = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.reloadImage)).BeginInit();
             this.SuspendLayout();
             // 
             // Question
@@ -42,12 +45,12 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Question.AutoSize = true;
-            this.Question.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Question.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 12.8F);
             this.Question.Location = new System.Drawing.Point(10, 6);
             this.Question.Margin = new System.Windows.Forms.Padding(10, 6, 10, 6);
             this.Question.MaximumSize = new System.Drawing.Size(757, 0);
             this.Question.Name = "Question";
-            this.Question.Size = new System.Drawing.Size(756, 174);
+            this.Question.Size = new System.Drawing.Size(756, 219);
             this.Question.TabIndex = 1;
             this.Question.Text = "Question goes here, lorem ipsum dolor sit amen?";
             this.Question.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -64,12 +67,12 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.Subject, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(123, 12);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(166, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(546, 107);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(459, 69);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // Subject
@@ -78,12 +81,12 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Subject.AutoSize = true;
-            this.Subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Subject.Location = new System.Drawing.Point(22, 6);
-            this.Subject.Margin = new System.Windows.Forms.Padding(22, 6, 0, 0);
+            this.Subject.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.Subject.Location = new System.Drawing.Point(0, 0);
+            this.Subject.Margin = new System.Windows.Forms.Padding(0);
             this.Subject.MaximumSize = new System.Drawing.Size(500, 95);
             this.Subject.Name = "Subject";
-            this.Subject.Size = new System.Drawing.Size(500, 95);
+            this.Subject.Size = new System.Drawing.Size(459, 69);
             this.Subject.TabIndex = 0;
             this.Subject.Text = "Subject";
             this.Subject.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -100,13 +103,23 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.Controls.Add(this.Question, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 143);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 98);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(776, 186);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(776, 231);
             this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // reloadImage
+            // 
+            this.reloadImage.BackColor = System.Drawing.Color.CadetBlue;
+            this.reloadImage.Location = new System.Drawing.Point(697, 15);
+            this.reloadImage.Name = "reloadImage";
+            this.reloadImage.Size = new System.Drawing.Size(67, 66);
+            this.reloadImage.TabIndex = 3;
+            this.reloadImage.TabStop = false;
+            this.reloadImage.Click += new System.EventHandler(this.reloadImage_Click);
             // 
             // Form1
             // 
@@ -114,8 +127,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSkyBlue;
             this.ClientSize = new System.Drawing.Size(800, 341);
+            this.Controls.Add(this.reloadImage);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(818, 388);
             this.MinimumSize = new System.Drawing.Size(818, 388);
             this.Name = "Form1";
@@ -125,6 +140,7 @@
             this.tableLayoutPanel1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.reloadImage)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -134,6 +150,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label Subject;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.PictureBox reloadImage;
     }
 }
 
--- Question_Gen_VS/Question_Gen_VS/Form1.cs
+++ Question_Gen_VS/Question_Gen_VS/Form1.cs
@@ -12,6 +12,13 @@ namespace Question_Gen_VS
 {
     public partial class Form1 : Form
     {
+        private void newQuestion()
+        {
+            Question current_question = new Question();
+            Subject.Text = current_question.subject;
+            Question.Text = current_question.question;
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -19,9 +26,7 @@ namespace Question_Gen_VS
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Question current_question = new Question();
-            Subject.Text = current_question.subject;
-            Question.Text = current_question.question;
+            newQuestion();
         }
 
         private void subject_Click(object sender, EventArgs e)
@@ -43,17 +48,77 @@ namespace Question_Gen_VS
         {
 
         }
+
+        private void reloadImage_Click(object sender, EventArgs e)
+        {
+            newQuestion();
+        }
     }
 
     class Question
     {
         private static Random rnd = new Random();
-        private static string[,] QuestionBank =
+        private static readonly string[,] QuestionBank =
         {
-        {"Physics", "question1"},
-        {"Comp Sci", "question2"},
-        {"Maths", "question3"},
-        {"Geo", "question4"}
+            //{"Maths", "text"},
+
+
+            //{"Physics", "text"},
+
+
+            //{"Comp Sci", "text"},
+        {"Comp Sci", "Identify two roles that a computer can perform in a network.  [2]"},
+        {"Comp Sci", "(a)   Identify  two reasons why patches may be necessary for an operating system.  [2]\n(b)  Identify two methods that can be used to obtain these patches.  [2]"},
+        {"Comp Sci", "Calculate the denary (base 10) equivalent of the hexadecimal number BF.  [2]"},
+        {"Comp Sci", "Identify two reasons why fibre optic cable would be preferred over wireless connectivity.  [2]"},
+        {"Comp Sci", "Distinguish between a variable and a constant. [2]"},
+        {"Comp Sci", "Identify one advantage of using a dedicated operating system on a mobile phone.  [1]"},
+        {"Comp Sci", "Identify two characteristics of a dynamic data structure.  [2]"},
+        {"Comp Sci", "Sketch a balanced binary tree that would allow the following output when traversed using inorder traversal:Zebra, Tango, Hotel, Foxtrot, Delta, Bravo, Alpha.  [3]"},
+        {"Comp Sci", "A school currently has a cabled network but wants to add wireless networking across the whole campus.\n\n(a)  Describe two hardware components the school will need to implement the wireless network.  [4]"},
+        {"Comp Sci", "A school currently has a cabled network but wants to add wireless networking across the whole campus.\n\n(b)  Identify two advantages to the students of the new wireless network.  [2]"},
+        {"Comp Sci", "There are concerns that unauthorized people could access the data on the wireless network. \n\n(c)  Outline two methods the school could employ to prevent network data from being accessed over their wireless system.  [4]"},
+        {"Comp Sci", "The school has decided to implement a virtual private network (VPN) to provide access  to its network.\n\n(d)  Identify two technologies the school would require to provide a VPN.  [2]"},
+        {"Comp Sci", "(e)  Explain one benefit to the staff of using a VPN to remotely access the school network.  [3]"},
+        {"Comp Sci", "The company’s staff list is now organized in the arrays in alphabetical order.\nA binary search was used to find a specific name in the array.\n\n(c)  Describe the process a binary search would follow to find a record in the surname array.  [4]\n(d)  Outline one benefit of using sub-programmes to implement your algorithms from parts (a) and (b).  [2]"},
+        {"Comp Sci", "A business has a range of different computers within the organization, including laptops, desktops and file servers. Wherever possible the organization uses a common operating system on its computers.\n\n(a)  Outline two resource management techniques that are likely to be carried out by the operating system of a desktop computer.  [4]"},
+        {"Comp Sci", "(b)  Outline one way the operating system hides the complexity of the hardware from the computer user.  [2]"},
+        {"Comp Sci", "Memory requirements and processor speed will vary depending on the tasks required of the computer.\n\n(c)  (i)   Contrast the memory requirements of a laptop computer and a file server.  [2]"},
+        {"Comp Sci", "(ii)  Contrast the processor speed requirements of a laptop computer and a file server.  [2]"},
+        {"Comp Sci", "The business has decided to implement a computer-based system to switch the room lights on and off automatically. The lights will only be switched on if the level of light is below a specific reading and there are people in the room. The lights will be switched off when the room has been unoccupied for at least five minutes.\n\n(d)  State two types of sensor that are required to control the lighting to ensure it switches on when it is required.  [2]"},
+        {"Comp Sci", "The business has decided to implement a computer-based system to switch the room lights on and off automatically. The lights will only be switched on if the level of light is below a specific reading and there are people in the room. The lights will be switched off when the room has been unoccupied for at least five minutes.\n\n(e)  Explain how the system makes use of the data it receives from the sensors to determine when to switch the lights on.  [4]"},
+        {"Comp Sci", "The business has decided to implement a computer-based system to switch the room lights on and off automatically. The lights will only be switched on if the level of light is below a specific reading and there are people in the room. The lights will be switched off when the room has been unoccupied for at least five minutes.\n\n(f)  Outline how the system will prevent the lights from being switched off too quickly when it thinks the room is unoccupied.  [2]"},
+        {"Comp Sci", "A network is set up with shared printers so that when a user wishes to print, print jobs are sent to a queue until the printer is available.\n\n(a)  Outline why a queue is the appropriate data structure to manage print jobs.  [2]"},
+        {"Comp Sci", "(b)  Draw a diagram to show how a print queue may be implemented using a linked list.  [3]"},
+        {"Comp Sci", "(c)  Explain why a stack would not be appropriate as a data structure for managing print jobs.  [3]"},
+        {"Comp Sci", "(e)  Explain how a stack may be used in the implementation of a recursive function.  [4]"},
+        {"Comp Sci", "Human interaction with the computer system includes a range of usability problems.\n\n(a) Definethetermusability. [1]"},
+        {"Comp Sci", "(b) Identify two methods that could be used to improve the accessibility of a computer system.  [2]"},
+        {"Comp Sci", "By making direct reference to the technologies used, explain how a virtual private network (VPN) allows a travelling salesperson to connect securely to their company’s network. [4]"},
+        {"Comp Sci", "Construct a truth table for the following Boolean expression. [3]\n\n(A and B) nor C"},
+        {"Comp Sci", "A school uses a local area network (LAN) which connects several computers and a printer to a server and allows access to the internet.\n\n(b)  Identify the different clients in this network. [1]"},
+        {"Comp Sci", "(c) (i)  Identify one external threat to the security of the school’s computer system. [1] \n\n (ii) State one way to protect the computer system from the threat identified in part (c)(i). [1]"},
+        {"Comp Sci", "(a)  Draw an annotated diagram showing how an array can be used to store a stack. [2]"},
+        {"Comp Sci", "(b)  Explain how elements in the stack may be reversed using a queue. [4]"},
+        {"Comp Sci", "(a) Definethetermserver. [1]"},
+        {"Comp Sci", "A hardware shop supplies a wide variety of bathroom equipment.  There are 15 shop assistantswhoservecustomers,3officestaffwhohandletheadministration,andamanager.  A specialized company is asked to design and implement a new computer system for  the shop. \n\n (a) (i)  Identify two different types of users of the system.  [2]  \n (ii)  Explain the role of users in the process of developing the new computer system. [3]"},
+        {"Comp Sci", "(b)  Describe why it is useful to produce more than one prototype of the new system.  [2]"},
+        {"Comp Sci", "(c) Outline two problems that may occur when transferring data from the old system to the new system. [4]"},
+        {"Comp Sci", "The manager of the shop has decided to invest in a computer system which allows customers to make online orders from any place at any time. \n\n (d)  (i)  Explain how two or more customers are able to access the computer system at the same time.  [2]  \n  (ii)  Explain how each customer’s data is secure when two customers access the system at the same time.  [2]"},
+        {"Comp Sci", "Outline the function of the:  \n\n  (i)  memory address register [2]"},
+        {"Comp Sci", "Outline the function of the:  \n\n  (ii)  memory data register.  [2]"},
+        {"Comp Sci", "(d) (i)  Identify two functions of the operating system.  [2]"},
+        {"Comp Sci", "(ii)  State where the operating system is held when the computer is turned off.   [1]"},
+        {"Comp Sci", "(e)  (i)  Construct a diagram to illustrate the structure of a central processing unit (CPU), clearlyshowingtheflowofdatawithintheCPU. [4]"},
+        {"Comp Sci", "(ii)  Identify the part of the CPU which performs decoding.  [1]"},
+        {"Comp Sci", "(iii)  Identify the part of the CPU which executes the instruction. [1]"},
+        {"Comp Sci", "A control system is used to control sliding doors which automatically open to allow people in and out of a shop. \n\n (a) (i)  Identify one type of sensor in this system. [1]"},
+        {"Comp Sci", "(ii) Identify one piece of hardware, other than sensors, that is part of the control system. [1]"},
+        {"Comp Sci", "(iii)  With reference to the role of sensors, outline the sequence of steps within the computer control system that will take place when a person approaches the door. (Sliding Automated door) [3]"},
+        {"Comp Sci", "(b) (i) Define the term interrupt.   [2] \n (ii)  Describe a situation in this system where an interrupt would occur. (Automated Sliding Doors)   [2]"},
+        {"Comp Sci", "(c)  Discuss the contribution of computer control systems in industry where they replace human workers. [6]"},
+
+        {"Comp Sci", "Describe one method of implementation for a new computer system.  [2]" }
     };
 
         public string subject;
@@ -61,7 +126,7 @@ namespace Question_Gen_VS
 
         public Question()
         {
-            int num = rnd.Next(0, 4);
+            int num = rnd.Next(0, QuestionBank.GetLength(0));
             subject = QuestionBank[num, 0];
             question = QuestionBank[num, 1];
         }
